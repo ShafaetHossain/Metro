5eba0027927c57b4b6a67a055d8feb528d0f56ae
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-09 00:46:24 +0600

    update schedule entity

--- src/Metro.Core/Entities/Schedule.cs
+++ src/Metro.Core/Entities/Schedule.cs
@@ -4,13 +4,14 @@ namespace Metro.Core.Entities
 {
     public class Schedule : BaseEntity<Guid>
     {
-        public Guid StationFrom { get; set; } //which is station ID
-        public Guid StationTo { get; set; } //which is station ID
+        public Guid StationFromId { get; set; } //which is station ID
+        public Guid StationToId { get; set; } //which is station ID
         public DateTime DepartureTime { get; set; }
         public int TotalSeat { get; set; }
         public int SeatBooked { get; set; }
         public int Price { get; set; }
 
-        public Station Station { get; set; } 
+        public virtual Station StationFrom { get; set; }
+        public virtual Station StationTo { get; set; }
     }
 }
