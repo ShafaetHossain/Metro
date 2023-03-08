06ba18b54d808f45522a16998da4d03e412fbf38
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-08 19:41:31 +0600

    add interface and class for statationCommand and baseCommand

---
+++ src/Metro.Infrastructure/Repository/Command/StationCommandRepository.cs
@@ -0,0 +1,11 @@
+﻿using Metro.Application.Contracts.Repositories.Command;
+using Metro.Core.Entities;
+using Metro.Infrastructure.Persistence;
+
+namespace Metro.Infrastructure.Repository.Command
+{
+    public class StationCommandRepository : CommandRepository<Station>, IStationCommandRepository
+    {
+        public StationCommandRepository(DbFactory dbFactory) : base(dbFactory) { }
+    }
+}
