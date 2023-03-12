943da4f0ac3d70e4e57a58a019e56483ad8ef9b5
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 15:34:20 +0600

    add interface & class for schedule command repo

---
+++ src/Metro.Infrastructure/Repository/Command/ScheduleCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Metro.Application.Contracts.Repositories.Command;
+using Metro.Core.Entities;
+using Metro.Infrastructure.Persistence;
+using Metro.Infrastructure.Repository.Command.Base;
+
+namespace Metro.Infrastructure.Repository.Command
+{
+    public class ScheduleCommandRepository : CommandRepository<Schedule>, IScheduleCommandRepository
+    {
+        public ScheduleCommandRepository(DbFactory dbFactory) : base(dbFactory) { }
+    }
+}
