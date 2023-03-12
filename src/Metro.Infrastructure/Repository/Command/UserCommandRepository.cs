1c0dd285c74f3b7e33331ebf59a87561b702b30c
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-13 00:31:54 +0600

    add interface and class for user command repo

---
+++ src/Metro.Infrastructure/Repository/Command/UserCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Metro.Application.Contracts.Repositories.Command;
+using Metro.Core.Entities;
+using Metro.Infrastructure.Persistence;
+using Metro.Infrastructure.Repository.Command.Base;
+
+namespace Metro.Infrastructure.Repository.Command
+{
+    public class UserCommandRepository : CommandRepository<User>, IUserCommandRepository
+    {
+        public UserCommandRepository(DbFactory dbFactory) : base(dbFactory) { }
+    }
+}
