d670fb2adf1245e2da85339fe2a3c61708fa35e7
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 16:19:56 +0600

    add interface and class for schedule query repo

---
+++ src/Metro.Application/Contracts/Repositories/Query/IScheduleQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿using Metro.Application.Contracts.Repositories.Query.Base;
+using Metro.Application.Queries.Schedules;
+using Metro.Core.Entities;
+
+namespace Metro.Application.Contracts.Repositories.Query
+{
+    public interface IScheduleQueryRepository : IMultipleResultQueryRepository<Schedule>
+    {
+        Task<(long, IEnumerable<Schedule>)> GetAllAsync(GetAllScheduleQuery request);
+    }
+}
---
+++ src/Metro.Infrastructure/Repository/Query/ScheduleQueryRepository.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using Metro.Application.Contracts.Repositories.Query;
+using Metro.Application.Queries.Schedules;
+using Metro.Core.Entities;
+using Metro.Core.Models;
+using Metro.Infrastructure.Configs;
+using Metro.Infrastructure.Repository.Query.Base;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Metro.Infrastructure.Repository.Query
+{
+    public class ScheduleQueryRepository : MultipleResultQueryRepository<Schedule>, IScheduleQueryRepository
+    {
+        public ScheduleQueryRepository(IConfiguration configuration, IOptions<MetroSettings> settings) : base(configuration, settings) 
+        { 
+        }
+
+        public async Task<(long, IEnumerable<Schedule>)> GetAllAsync(GetAllScheduleQuery queryParams)
+        {
+            int pageSize = queryParams.PageSize;
+            int offset = (queryParams.PageNumber - 1) * pageSize;    //offset = (pageNumber -1) * pageSize
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@offset", offset);
+            parameters.Add("@pageSize", pageSize);
+
+            string query = @"declare @totalCount bigint
+                            SELECT @totalCount = count(sch.Id)
+                            FROM Schedules AS sch
+                            WHERE sch.IsDeleted = 0
+
+                            SELECT sch.Id, sch.StationFromId,
+                            sch.StationToId, sch.DepartureTime,
+                            sch.TotalSeat, sch.SeatBooked, sch.Price,
+                            @totalCount AS TotalCount FROM Schedules
+                            AS sch WHERE sch.IsDeleted = 0";
+
+            string filter = "";
+
+            //filter query condition is implemented if filter string length is greater than 0
+            if (filter.Length > 0) query += filter;
+
+            //order by 
+            query += " ORDER BY st.CreatedDate DESC, st.LastModifiedDate DESC ";
+
+            //pagination condition is implemented only if pageSize and offset value is valid
+            if (pageSize > 1 && offset >= 0)
+            {
+                query += " OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY";
+            }
+            var _totalCount = 0;
+            var scheduleList = await base.GetAsync<Schedule, TotalDataCount, Schedule>(query,
+                (schedule, count) =>
+                {
+                    _totalCount = count.TotalCount;
+                    return schedule;
+                }, parameters, splitters: "TotalCount", false);
+
+            return (_totalCount, scheduleList);
+        }
+    }
+}
