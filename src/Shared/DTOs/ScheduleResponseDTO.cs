b19dc2311cbebd814dbe14c91841a1f76205498b
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 15:32:43 +0600

    create dto for schedule

---
+++ src/Shared/DTOs/ScheduleResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.DTOs
+{
+    public class ScheduleResponseDTO
+    {
+        public Guid Id { get; set; }
+        public Guid StationFromId { get; set; }
+        public Guid StationToId { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TotalSeat { get; set; }
+        public int SeatBooked { get; set; }
+        public int Price { get; set; }
+    }
+}
