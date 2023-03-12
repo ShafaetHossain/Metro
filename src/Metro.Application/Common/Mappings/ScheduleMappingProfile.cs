42744c5d5de0fbe9f6293714122b647322b292ce
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 15:36:58 +0600

    add mapping for scheule

---
+++ src/Metro.Application/Common/Mappings/ScheduleMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Metro.Core.Entities;
+using Shared.Commands.Schedules;
+using Shared.DTOs;
+
+namespace Metro.Application.Common.Mappings
+{
+    public class ScheduleMappingProfile : Profile
+    {
+        public ScheduleMappingProfile()
+        {
+            //DTO to Domain
+            CreateMap<Schedule, ScheduleResponseDTO>().ReverseMap();
+
+            //Command to Domain
+            CreateMap<Schedule, CreateScheduleCommand>().ReverseMap();
+        }
+    }
+}
