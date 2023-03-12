7bb2dbecf509ea563c4f1b369904fff71d353314
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 18:28:13 +0600

    add handler for update schedule

---
+++ src/Metro.Application/CommandHandlers/Schedules/UpdateScheduleCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MediatR;
+using Metro.Application.Common.Exceptions;
+using Metro.Application.Contracts.Repositories;
+using Metro.Application.Contracts.Repositories.Command;
+using Metro.Application.Contracts.Repositories.Query;
+using Metro.Core.Entities;
+using Shared.Commands.Schedules;
+using Shared.DTOs;
+
+namespace Metro.Application.CommandHandlers.Schedules
+{
+    public class UpdateScheduleCommandHandler : IRequestHandler<UpdateScheduleCommand, ScheduleResponseDTO>
+    {
+        private readonly IScheduleCommandRepository _scheduleCommandRepository;
+        private readonly IScheduleQueryRepository _scheduleQueryRepository;
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateScheduleCommandHandler(IScheduleCommandRepository scheduleCommandRepository, IScheduleQueryRepository scheduleQueryRepository, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _scheduleCommandRepository = scheduleCommandRepository;
+            _scheduleQueryRepository = scheduleQueryRepository;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ScheduleResponseDTO> Handle(UpdateScheduleCommand request, CancellationToken cancellationToken)
+        {
+            var currentSchedule = await _scheduleQueryRepository.GetByIdAsync(request.Id);
+            if(currentSchedule == null)
+            {
+                throw new NotFoundException("Schedule not found");
+            }
+            //map request type to entity(table) type so that we can insert
+            var scheduleEntity = _mapper.Map<UpdateScheduleCommand, Schedule>(request, currentSchedule);
+
+            //update the entity and collect the response
+            var updateScheduleEntity = _scheduleCommandRepository.UpdateAsync(scheduleEntity);
+
+            //commit changes
+            await _unitOfWork.CommitAsync(cancellationToken);
+
+            return _mapper.Map<ScheduleResponseDTO>(updateScheduleEntity.Result);
+        }
+    }
+}
