d8039378ef96921d91d30609a46a34ae6e97474d
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 18:26:34 +0600

    add command obj for update schedule

---
+++ src/Shared/Commands/Schedules/UpdateScheduleCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Shared.DTOs;
+
+namespace Shared.Commands.Schedules
+{
+    public class UpdateScheduleCommand : IRequest<ScheduleResponseDTO>
+    {
+        public Guid Id { get; set; }
+        public Guid StationFromId { get; set; }
+        public Guid StationToId { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TotalSeat { get; set; }
+        public int SeatBooked { get; set; }
+        public int Price { get; set; }
+    }
+}
