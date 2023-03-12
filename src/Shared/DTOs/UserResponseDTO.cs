45f6c37334212181a2e53b9cb7908f9ca32cfe67
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-13 00:08:02 +0600

    add user response class

---
+++ src/Shared/DTOs/UserResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.DTOs
+{
+    public class UserResponseDTO
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
