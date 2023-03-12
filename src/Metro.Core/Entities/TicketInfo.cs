0d376b8ad5101de65a4c6d33c34155d9c004d8e9
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-13 00:02:36 +0600

    modify entities

--- src/Metro.Core/Entities/TicketInfo.cs
+++ src/Metro.Core/Entities/TicketInfo.cs
@@ -1,9 +1,4 @@
 ﻿using Metro.Core.Entities.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Metro.Core.Entities
 {
--- src/Metro.Core/Entities/User.cs
+++ src/Metro.Core/Entities/User.cs
@@ -7,5 +7,6 @@ namespace Metro.Core.Entities
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
