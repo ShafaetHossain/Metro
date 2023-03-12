8191a30235bcedd533370e5cbd6c84bf2c629fba
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 10:24:10 +0600

    add a command class for update station

---
+++ src/Shared/Commands/Stations/UpdateStationCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Shared.DTOs;
+
+namespace Shared.Commands.Stations
+{
+    public class UpdateStationCommand : IRequest<StationResponseDTO>
+    {
+        public Guid Id {  get; set; }
+        public string StationName { get; set; } 
+    }
+}
