d4599c67f6901eb3d64ba71786bcb2ca7efac666
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 15:33:29 +0600

    create class for schedule command(request object)

---
+++ src/Shared/Commands/Schedules/CreateScheduleCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Shared.DTOs;
+
+namespace Shared.Commands.Schedules
+{
+    public class CreateScheduleCommand : IRequest<ScheduleResponseDTO>
+    {
+        public Guid StationFromId { get; set; }
+        public Guid StationToId { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TotalSeat { get; set; }
+        public int SeatBooked { get; set; }
+        public int Price { get; set; }
+    }
+}
