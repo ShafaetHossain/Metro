7cece7b8bd356def9eb1847c6b4ee14ab30c730d
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-13 00:31:09 +0600

    add command for user create

---
+++ src/Shared/Commands/Users/CreateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Shared.DTOs;
+
+namespace Shared.Commands.Users
+{
+    public class CreateUserCommand : IRequest<UserResponseDTO>
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
