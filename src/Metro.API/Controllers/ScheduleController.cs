3f351756b3c1dda74724339252ec7a0ddf94cff5
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-12 15:37:14 +0600

    add endpoint for create schedule

---
+++ src/Metro.API/Controllers/ScheduleController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Commands.Schedules;
+
+namespace Metro.API.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    //[Authorize]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ScheduleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSchedule([FromBody] CreateScheduleCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
