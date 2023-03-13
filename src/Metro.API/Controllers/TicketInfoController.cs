bbabba8277b7e033c8c5cd0c69eed9ef0e467b5f
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-14 01:28:30 +0600

    add new controller for ticket

---
+++ src/Metro.API/Controllers/TicketInfoController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Commands.TicketInfoes;
+
+namespace Metro.API.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    //[Authorize]
+    [ApiController]
+    public class TicketInfoController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TicketInfoController(IMediator mediator) 
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("buyTicket")]
+        public async Task<IActionResult> BuyTicket([FromBody] CreateTicketInfoCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+    }
+}
