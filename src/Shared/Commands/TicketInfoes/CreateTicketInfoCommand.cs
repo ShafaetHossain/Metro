5c3672356221c292f7bdd35d11daaa31e0e9674c
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-14 01:25:17 +0600

    add command for create ticketInfo

---
+++ src/Shared/Commands/TicketInfoes/CreateTicketInfoCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Shared.DTOs;
+
+namespace Shared.Commands.TicketInfoes
+{
+    public class CreateTicketInfoCommand : IRequest<TicketInfoResponseDTO>
+    {
+        public Guid ScheduleId { get; set; }
+        public Guid UserId { get; set; }
+        public int BuySeat { get; set; }
+    }
+}
