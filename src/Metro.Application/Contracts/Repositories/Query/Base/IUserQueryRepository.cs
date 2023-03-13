65179e178e6f606007c77b2dee64bd009bf91f28
Md. Shafaet Hossain <shafaet.bd@asa-international.com>
2023-03-13 16:09:01 +0600

    add interface and class user query repo

---
+++ src/Metro.Application/Contracts/Repositories/Query/Base/IUserQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Metro.Core.Entities;
+using Shared.Commands.Users;
+
+namespace Metro.Application.Contracts.Repositories.Query.Base
+{
+    public interface IUserQueryRepository : IMultipleResultQueryRepository<User>
+    {
+        Task<User> GetByEmailAsync(AuthenticateUserCommand request);
+    }
+}
---
+++ src/Metro.Infrastructure/Repository/Query/UserQueryRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Metro.Application.Contracts.Repositories.Query.Base;
+using Metro.Core.Entities;
+using Metro.Infrastructure.Configs;
+using Metro.Infrastructure.Repository.Query.Base;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Shared.Commands.Users;
+
+namespace Metro.Infrastructure.Repository.Query
+{
+    public class UserQueryRepository : MultipleResultQueryRepository<User>, IUserQueryRepository
+    {
+        public UserQueryRepository(IConfiguration configuration, IOptions<MetroSettings> settings) : base(configuration, settings)
+        {
+        }
+
+        public async Task<User> GetByEmailAsync(AuthenticateUserCommand request)
+        {
+            DynamicParameters parameters = new DynamicParameters();
+            parameters.Add("@Email", request.Email);
+            parameters.Add("@Password", request.Password);
+
+            string query = "SELECT u.Id, u.UserName, " +
+                           "u.Email, u.Password, u.Role " +
+                           "FROM Users AS u WHERE Email = @Email " +
+                           "AND Password = @Password AND IsDeleted = 0";
+
+            return await SingleAsync(query, parameters);
+        }
+    }
+}
