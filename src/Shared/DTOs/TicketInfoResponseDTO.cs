cbc0e2f1bc1965f5fbb791f375d4732ac12ccada
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-14 01:24:51 +0600

    add dto for ticketInfo

---
+++ src/Shared/DTOs/TicketInfoResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.DTOs
+{
+    public class TicketInfoResponseDTO
+    {
+        public Guid Id { get; set; }
+        public Guid ScheduleId { get; set; }
+        public Guid UserId { get; set; }
+        public int BuySeat { get; set; }
+    }
+}
