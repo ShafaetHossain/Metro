e8cb09ab13b9c599e6df704e25b224033e60fc78
Md. Shafaet Hossain <shafaet.bd@asa-international.com>
2023-03-13 16:08:17 +0600

    add a class for all validation error

---
+++ src/Metro.Application/Common/Constants/ValidationError.cs
@@ -0,0 +1,9 @@
+﻿namespace Metro.Application.Common.Constants
+{
+    public class ValidationError
+    {
+        public const string UserLoginSuccess = "Login success!",
+                            UserNotFound = "User not found!",
+                            UserRegistered = "Registered successfully!";
+    }
+}
