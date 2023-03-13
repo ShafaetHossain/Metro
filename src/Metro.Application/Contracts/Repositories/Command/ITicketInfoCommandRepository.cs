f416951cc37b5642f66b3566c23a078d1e4bcb4f
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-14 01:26:03 +0600

    add interface and class for ticketinfo repo

---
+++ src/Metro.Application/Contracts/Repositories/Command/ITicketInfoCommandRepository.cs
@@ -0,0 +1,9 @@
+﻿using Metro.Application.Contracts.Repositories.Command.Base;
+using Metro.Core.Entities;
+
+namespace Metro.Application.Contracts.Repositories.Command
+{
+    public interface ITicketInfoCommandRepository : ICommandRepository<TicketInfo>
+    {
+    }
+}
---
+++ src/Metro.Infrastructure/Repository/Command/TicketInfoCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using Metro.Application.Contracts.Repositories.Command;
+using Metro.Core.Entities;
+using Metro.Infrastructure.Persistence;
+using Metro.Infrastructure.Repository.Command.Base;
+
+namespace Metro.Infrastructure.Repository.Command
+{
+    public class TicketInfoCommandRepository : CommandRepository<TicketInfo>, ITicketInfoCommandRepository
+    {
+        public TicketInfoCommandRepository(DbFactory dbFactory) : base(dbFactory) { }
+    }
+}
