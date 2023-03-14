a7d2fb6ddeac7cdd0236ada7b8e6605f393b602f
Md. Shafaet Hossain <shafaet.bd@asa-international.com>
2023-03-14 15:08:59 +0600

    update login components

--- web/src/app/modules/authentication/pages/login/login.component.ts
+++ web/src/app/modules/authentication/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  type: string = "password"
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
+
+  loginForm!:FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    //  private userservice: UserService,
+      // private router: Router,
+      // private toast: NgToastService
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onClickLogin(){
+    if(this.loginForm.valid){
+      
+    }
+    else{
+      console.log("Failed");
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+
+  public testing: boolean = true;
 
 }
