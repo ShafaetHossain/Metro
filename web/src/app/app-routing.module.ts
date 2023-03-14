d597d8c6ff43d7526cc67d8bd6330781c1312ad2
Md. Shafaet Hossain <shafaet.bd@asa-international.com>
2023-03-14 15:09:56 +0600

    update app and package

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/authentication/pages/login/login.component';
+import { RegisterComponent } from './modules/authentication/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: 'dashboard',
+  //   component: Dashboard,
+  //   data: {
+  //     title: 'Dashboard'
+  //   }
+  // }
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register Page'
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/authentication/pages/login/login.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgToastModule } from 'ng-angular-popup';
+import { Navigation } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
