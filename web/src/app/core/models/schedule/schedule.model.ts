f597a1a765607946d654d97d372653ed22aef854
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-15 18:24:22 +0600

    create station and schedule models

---
+++ web/src/app/core/models/schedule/schedule.model.ts
@@ -0,0 +1,9 @@
+export class Schedule {
+  id!: string;
+  stationFromId!: string;
+  stationToId!: string;
+  departureTime!: string;
+  totalSeat!: number;
+  seatBooked!: number;
+  price!: number;
+}
