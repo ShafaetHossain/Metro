54def83f7af5d681d6e77b03e4054c6b405d98d5
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-16 05:04:04 +0600

    add schedule test controller

---
+++ tests/Metro.API.UnitTests/Controllers/ScheduleControllerTests.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using MediatR;
+using Metro.API.Controllers;
+using Metro.API.UnitTests.Constants;
+using Metro.Application.Queries.Schedules;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Shared.Commands.Schedules;
+using Xunit;
+
+namespace Metro.API.UnitTests.Controllers
+{
+    public class ScheduleControllerTests
+    {
+        private readonly Mock<IMediator> _mediatorMock;
+        private readonly ScheduleController _controller;
+
+        public ScheduleControllerTests()
+        {
+            _mediatorMock = new Mock<IMediator>();
+            _controller = new ScheduleController(_mediatorMock.Object);
+        }
+
+        [Fact]
+        public async Task GetAllSchedule_Should_Return_Success_Response()
+        {
+            // arrange
+            var allScheduleQuery = new GetAllScheduleQuery();
+
+            // act
+            var result = (OkObjectResult)await _controller.GetAllSchedule(allScheduleQuery);
+
+            // assert
+            result.StatusCode.Should().Be(HttpStatusConstants.StatusCode200);
+        }
+
+        [Fact]
+        public async Task GetScheduleById_Should_Return_Success_Response()
+        {
+            // arrange
+            var id = new Guid();
+
+            // act
+            var result = (OkObjectResult)await _controller.GetScheduleById(id);
+
+            // assert
+            result.StatusCode.Should().Be(HttpStatusConstants.StatusCode200);
+        }
+
+        [Fact]
+        public async Task CreateSchedule_Should_Return_Success_Response()
+        {
+            // arrange
+            var scheduleCreateCommand = new CreateScheduleCommand();
+
+            // act
+            var result = (OkObjectResult)await _controller.CreateSchedule(scheduleCreateCommand);
+
+            // assert
+            result.StatusCode.Should().Be(HttpStatusConstants.StatusCode200);
+        }
+
+        [Fact]
+        public async Task UpdateSchedule_Should_Return_Success_Response()
+        {
+            // Arrange
+            var id = new Guid();
+            var scheduleUpdateCommand = new UpdateScheduleCommand();
+
+            // Act
+            var result = (OkObjectResult)await _controller.UpdateSchedule(id, scheduleUpdateCommand);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusConstants.StatusCode200);
+        }
+
+        [Fact]
+        public async Task DeleteSchedule_Should_Return_Success_Response()
+        {
+            // Arrange
+            var guid = new Guid();
+
+            // Act
+            var result = (OkObjectResult)await _controller.DeleteSchedule(guid);
+
+            // Assert
+            result.StatusCode.Should().Be(HttpStatusConstants.StatusCode200);
+        }
+    }
+}
