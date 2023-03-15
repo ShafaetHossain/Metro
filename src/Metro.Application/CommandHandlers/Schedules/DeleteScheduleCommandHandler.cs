dfb0345254ed2a99c13841a09e25559c20eed5e6
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-16 03:55:41 +0600

    add handler to delete entity for station and schedule

---
+++ src/Metro.Application/CommandHandlers/Schedules/DeleteScheduleCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Metro.Application.Common.Constants;
+using Metro.Application.Common.Exceptions;
+using Metro.Application.Contracts.Repositories;
+using Metro.Application.Contracts.Repositories.Command;
+using Metro.Application.Contracts.Repositories.Query;
+using Shared.Commands.Schedules;
+
+namespace Metro.Application.CommandHandlers.Schedules
+{
+    public class DeleteScheduleCommandHandler : IRequestHandler<DeleteScheduleCommand, string>
+    {
+        private readonly IScheduleCommandRepository _scheduleCommandRepository;
+        private readonly IScheduleQueryRepository _scheduleQueryRepository;
+        IUnitOfWork _unitOfWork;
+
+        public DeleteScheduleCommandHandler(IScheduleCommandRepository scheduleCommandRepository, IScheduleQueryRepository scheduleQueryRepository, IUnitOfWork unitOfWork)
+        {
+            _scheduleCommandRepository = scheduleCommandRepository;
+            _scheduleQueryRepository = scheduleQueryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<string> Handle(DeleteScheduleCommand request, CancellationToken cancellationToken)
+        {
+            var scheduleEntity = await _scheduleQueryRepository.GetByIdAsync(request.Id);
+            if(scheduleEntity == null) 
+            {
+                throw new NotFoundException(ValidationError.ScheduleNotFound);
+            }
+
+            scheduleEntity.IsDeleted = true;
+
+            await _scheduleCommandRepository.UpdateAsync(scheduleEntity);
+
+            await _unitOfWork.CommitAsync(cancellationToken);
+
+            return ValidationError.ScheduleDeleted;
+        }
+    }
+}
---
+++ src/Metro.Application/CommandHandlers/Stations/DeleteStationCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using Metro.Application.Common.Constants;
+using Metro.Application.Common.Exceptions;
+using Metro.Application.Contracts.Repositories;
+using Metro.Application.Contracts.Repositories.Command;
+using Metro.Application.Contracts.Repositories.Query;
+using Shared.Commands.Stations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metro.Application.CommandHandlers.Stations
+{
+    public class DeleteStationCommandHandler : IRequestHandler<DeleteStationCommand, string>
+    {
+        private readonly IStationCommandRepository _stationCommandRepository;
+        private readonly IStationQueryRepository _stationQueryRepositor;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteStationCommandHandler(IStationCommandRepository stationCommandRepository, IStationQueryRepository stationQueryRepositor, IUnitOfWork unitOfWork)
+        {
+            _stationCommandRepository = stationCommandRepository;
+            _stationQueryRepositor = stationQueryRepositor;
+            _unitOfWork= unitOfWork;
+        }
+
+        public async Task<string> Handle(DeleteStationCommand request, CancellationToken cancellationToken)
+        {
+            var stationEntity = await _stationQueryRepositor.GetByIdAsync(request.Id);
+            if(stationEntity == null)
+            {
+                throw new NotFoundException(ValidationError.StationNotFound);
+            }
+
+            stationEntity.IsDeleted = true;
+
+            await _stationCommandRepository.UpdateAsync(stationEntity);
+
+            await _unitOfWork.CommitAsync(cancellationToken);
+
+            return ValidationError.StationDeleted;
+        }
+    }
+}
