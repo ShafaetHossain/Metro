57b638dd5b518aefcaee31d5258ee183d4f27a77
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-15 18:24:53 +0600

    add data get service

--- web/src/app/core/services/http.service.ts
+++ web/src/app/core/services/http.service.ts
@@ -4,7 +4,7 @@ import {
   HttpErrorResponse,
   HttpHeaders,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +19,23 @@ export class HttpService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
+  get<T>(url: string, endpoint: string): Observable<T> {
+    return this.httpClient
+      .get<T>(`${url}/${endpoint}`)
+      .pipe(retry(0), catchError(this.handleError));
+  }
+
   post<T>(url: string, endpoint: string, item: any): Observable<T>{
     return this.httpClient
     .post<T>(`${url}/${endpoint}`, JSON.stringify(item), this.httpOptions)
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = '';
+
+    //error client
+    errorMessage = error.error ? error.error[0]?.message : 'Some error occured';
+
+    return throwError(errorMessage ?? 'Something went wrong');
   }
 }
