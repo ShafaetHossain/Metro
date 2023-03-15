ca52aa5ebe1e5265156bdcdbccdd7cd40e1b4e42
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-16 03:55:12 +0600

    add validation error message for station and schedule

--- src/Metro.Application/Common/Constants/ValidationError.cs
+++ src/Metro.Application/Common/Constants/ValidationError.cs
@@ -5,5 +5,11 @@
         public const string UserLoginSuccess = "Login success!",
                             UserNotFound = "User not found!",
                             UserRegistered = "Registered successfully!";
+
+        public const string StationNotFound = "Station not found!",
+                            StationDeleted = "Station information is deleted!";
+
+        public const string ScheduleNotFound = "Schedule not found!",
+                            ScheduleDeleted = "Schedule information is deleted!";
     }
 }
