008ea34690ddaa9a77895b823f0683712e63c114
Md. Shafaet Hossain <shafaet.bd@asa-international.com>
2023-03-16 12:03:35 +0600

    add test for station query

---
+++ tests/Metro.Application.UnitTests/Queries/Stations/GetAllStationQueryTests.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Metro.Application.Contracts.Repositories.Query;
+using Metro.Application.Queries.Stations;
+using Metro.Core.Entities;
+using Moq;
+using Shared.DTOs;
+using Xunit;
+
+namespace Metro.Application.UnitTests.Queries.Stations
+{
+    public class GetAllStationQueryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IStationQueryRepository> _stationQueryRepositoryMock;
+
+        public GetAllStationQueryTests()
+        {
+            // Initialize the AutoMapper configuration
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Station, StationResponseDTO>();
+            });
+
+            _mapper = config.CreateMapper();
+
+            // Initialize the mock repository
+            _stationQueryRepositoryMock = new Mock<IStationQueryRepository>();
+        }
+
+        [Fact]
+        public async Task Handle_Returns_PaginatedListResponseDTO_Of_StationResponseDTO()
+        {
+            // arrange
+            var query = new GetAllStationQuery
+            {
+                PageNumber = 1,
+                PageSize = 10,
+                StationName = "Uttara",
+            };
+
+            var stations = new List<Station>
+            {
+                new Station(),
+                new Station(),
+                new Station()
+            };
+            var count = 1L;
+
+            _stationQueryRepositoryMock.Setup(repo => repo.GetAllAsync(query))
+                .ReturnsAsync((count, stations));
+
+            var handler = new GetAllStationQueryHandler(_stationQueryRepositoryMock.Object, _mapper);
+
+            // act
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            // assert
+            Assert.NotNull(result);
+            Assert.IsType<PaginatedListResponseDTO<StationResponseDTO>>(result);
+        }
+    }
+}
---
+++ tests/Metro.Application.UnitTests/Queries/Stations/GetStationByIdQueryTests.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Metro.Application.Common.Mappings;
+using Metro.Application.Contracts.Repositories.Query;
+using Metro.Application.Queries.Stations;
+using Metro.Common.UnitTests.Mocks;
+using Moq;
+using Shared.DTOs;
+using Xunit;
+
+namespace Metro.Application.UnitTests.Queries.Stations
+{
+    public class GetStationByIdQueryTests
+    {
+        private readonly Mock<IStationQueryRepository> _stationQueryRepositoryMock;
+        private readonly IMapper _mapper;
+        private readonly GetStationByIdQueryHandler _handler;
+
+        public GetStationByIdQueryTests()
+        {
+            _stationQueryRepositoryMock = StationMockRepository.GetStationByIdQueryRepository();
+            _mapper = new MapperConfiguration(cfg => cfg.AddProfile<StationMappingProfile>()).CreateMapper();
+            _handler = new GetStationByIdQueryHandler(_stationQueryRepositoryMock.Object, _mapper);
+        }
+
+        [Fact]
+        public async Task Will_Return_NotNull_When_Response_Found()
+        {
+            Guid id = new Guid("40979b45-9ab6-40be-9a4d-7aa01d226d27");
+            GetStationByIdQuery response = new GetStationByIdQuery(id);
+            var result = await _handler.Handle(response, CancellationToken.None);
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Will_Return_True_When_Invalid_Type_IsNotMatched()
+        {
+            Guid id = new Guid("40979b45-9ab6-40be-9a4d-7aa01d226d27");
+            GetStationByIdQuery response = new GetStationByIdQuery(id);
+            var result = await _handler.Handle(response, CancellationToken.None);
+            result.Should().BeOfType<StationResponseDTO>();
+        }
+    }
+}
