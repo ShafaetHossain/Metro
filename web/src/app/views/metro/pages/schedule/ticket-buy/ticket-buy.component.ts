852bb90a64c35a6e1a88b0739dbbaa8320d11477
Md. Shafaet Hossain <Md. Shafaet Hossain@DESKTOP-GMRPKB3>
2023-03-17 12:05:40 +0600

    create new component for buy ticket

---
+++ web/src/app/views/metro/pages/schedule/ticket-buy/ticket-buy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-buy',
+  templateUrl: './ticket-buy.component.html',
+  styleUrls: ['./ticket-buy.component.scss']
+})
+export class TicketBuyComponent {
+
+}
